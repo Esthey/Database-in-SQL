c55508c587d809b477a72698584b7f343d55342f
Esther O <143701308+Esthey@users.noreply.github.com>
2023-09-19 04:05:15 -0700

    Add files via upload

---
+++ SQLQuery4.sql
@@ -0,0 +1,5 @@
+SELECT FirstName, LastName, Gender, Salary, 
+COUNT (Gender) OVER (PARTITION BY Gender) as TotalGender
+FROM [SQL TUTORIAL].[dbo].[Demo]
+JOIN [SQL TUTORIAL].[dbo].[Salary]
+ON [dbo].[Demo].EmployeeID= [dbo].[Salary].EmployeeID
